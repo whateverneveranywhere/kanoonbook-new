3d69b7858b4a9c28db9b575bd9a403ba3d0068d7
Ava Walker <firstexhotic@gmail.com>
2019-09-08 18:10:20 +0430

    main section is mostly done. TODO : work on testemonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,42 @@ let countDown = new Date('Sep 30, 2019 00:00:00').getTime(),
     function closeSearch() {
       document.getElementById("myOverlay").style.display = "none";
     }
+
+
+    // hide guide
+
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 1) { //use `this`, not `document`
+        $('.fixedelement').css({
+            'display': 'none'
+        });
+    }
+});
+
+
+  // scroller Smooth
+
+  $(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("#section1").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
